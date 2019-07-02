3ab669667c3e9d488b9d013732dab12b649664e0
JaiMistry <jsmistry@asu.edu>
2019-07-02 15:02:34 -0700

    Testing pulling data from Firestore

--- lib/pages/food.dart
+++ lib/pages/food.dart
@@ -2,12 +2,57 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class FoodPage extends StatelessWidget {
-  const FoodPage({Key key}) : super(key: key);
+  // const FoodPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return FoodBody();
+  }
+}
+
+class FoodBody extends StatefulWidget {
+  FoodBody({Key key}) : super(key: key);
+
+  _FoodBodyState createState() => _FoodBodyState();
+}
+
+class _FoodBodyState extends State<FoodBody> {
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Placeholder(),
+      child: StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection('food').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData | snapshot.hasError) {
+            return LinearProgressIndicator();
+            // return Text('Loading...');
+          }
+
+          print(snapshot.data.documents.length);
+
+          snapshot.data.documents.forEach((place) {
+            print(place.documentID);
+            print(place.data);
+          });
+          
+          return ListView.builder(
+            itemCount: snapshot.data.documents.length,
+            itemBuilder: (BuildContext context, int index) {
+              DocumentSnapshot resturant = snapshot.data.documents[index];
+              String resturantName = resturant.documentID.toString();
+
+              return ListTile(
+                title: Row(
+                  children: <Widget>[
+                    Text(resturantName),
+                    Image.network(resturant.data['image_url'], width: 200.0,)
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
