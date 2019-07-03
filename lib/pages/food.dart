6edf03c0e6f1c43c15d399f1a7b327e0032a67b1
JaiMistry <jsmistry@asu.edu>
2019-07-03 11:49:58 -0700

    Images now fade in after loading

--- lib/pages/food.dart
+++ lib/pages/food.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+import 'package:transparent_image/transparent_image.dart';
+
 class FoodPage extends StatelessWidget {
   // const FoodPage({Key key}) : super(key: key);
 
@@ -32,17 +34,37 @@ class _FoodBodyState extends State<FoodBody> {
       return Container();
     }
 
+    String imageUrl = restaurant.data['image_url'];
+    if (imageUrl == null) {
+      imageUrl = 'https://via.placeholder.com/500x500?text=Error+Loading+Image';
+    }
     return Container(
       padding: EdgeInsets.all(10),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(15.0),
         child: Container(
           color: Colors.white,
-          child: Image.network(
-            restaurant.data['image_url'],
-            width: 175,
-            height: 175,
+          child: Stack(
+            children: <Widget>[
+              // Center(child: CircularProgressIndicator()),
+              Center(
+                child: FadeInImage.memoryNetwork(
+                  placeholder: kTransparentImage,
+                  image: imageUrl,
+                  width: 175,
+                  height: 175,
+                  fit: BoxFit.contain,
+                  fadeInDuration: Duration(milliseconds: 400),
+                ),
+              ),
+            ],
           ),
+          // child: Image.network(
+          //   imageUrl,
+          //   width: 175,
+          //   height: 175,
+          //   fit: BoxFit.contain,
+          // ),
         ),
       ),
     );
@@ -56,7 +78,7 @@ class _FoodBodyState extends State<FoodBody> {
         stream: Firestore.instance.collection('food').snapshots(),
         builder: (context, snapshot) {
           if (!snapshot.hasData | snapshot.hasError) {
-            return LinearProgressIndicator();
+            return Container();
             // return Text('Loading...');
           }
           // print(snapshot.data.documents.length);
@@ -66,33 +88,43 @@ class _FoodBodyState extends State<FoodBody> {
           //   print(place.data);
           // });
 
-          return ListView.builder(
-            itemCount: snapshot.data.documents.length,
-            itemBuilder: (BuildContext context, int index) {
-              DocumentSnapshot restaurant = snapshot.data.documents[index];
-              DocumentSnapshot nextRestaurant;
-              // String restaurantName = restaurant.documentID.toString();
-              // print(index);
-              // print(snapshot.data.documents.length);
-              if (index + 1 < snapshot.data.documents.length) {
-                nextRestaurant = snapshot.data.documents[index + 1];
-              }
+          return ScrollConfiguration(
+            behavior: NoOverscrollBehavior(),
+            child: ListView.builder(
+              itemCount: snapshot.data.documents.length,
+              itemBuilder: (BuildContext context, int index) {
+                DocumentSnapshot restaurant = snapshot.data.documents[index];
+                DocumentSnapshot nextRestaurant;
+                // String restaurantName = restaurant.documentID.toString();
+                // print(index);
+                // print(snapshot.data.documents.length);
+                if (index + 1 < snapshot.data.documents.length) {
+                  nextRestaurant = snapshot.data.documents[index + 1];
+                }
 
-              if (index.isEven || index == snapshot.data.documents.length) {
-                return Row(
-                  mainAxisAlignment: nextRestaurant != null ? MainAxisAlignment.center : MainAxisAlignment.start,
-                  children: <Widget>[
-                    _makeFoodCard(restaurant),
-                    _makeFoodCard(nextRestaurant),
-                  ],
-                );
-              }
+                if (index.isEven || index == snapshot.data.documents.length) {
+                  return Row(
+                    mainAxisAlignment: nextRestaurant != null ? MainAxisAlignment.center : MainAxisAlignment.start,
+                    children: <Widget>[
+                      _makeFoodCard(restaurant),
+                      _makeFoodCard(nextRestaurant),
+                    ],
+                  );
+                }
 
-              return Container();
-            },
+                return Container();
+              },
+            ),
           );
         },
       ),
     );
+  }
+}
+
+class NoOverscrollBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
   }
 }
