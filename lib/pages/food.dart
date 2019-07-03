2e8d7a7104b5b85135d199a8e989b28afb2ba452
JaiMistry <jsmistry@asu.edu>
2019-07-03 11:25:04 -0700

    Styled dynamically build cards (WIP)

--- lib/pages/food.dart
+++ lib/pages/food.dart
@@ -6,7 +6,17 @@ class FoodPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FoodBody();
+    return Container(
+      child: FoodBody(),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xff170422), Color(0xff9B22E6)],
+          stops: [0.75, 1],
+        ),
+      ),
+    );
   }
 }
 
@@ -17,9 +27,31 @@ class FoodBody extends StatefulWidget {
 }
 
 class _FoodBodyState extends State<FoodBody> {
+  Widget _makeFoodCard(DocumentSnapshot restaurant) {
+    if (restaurant == null) {
+      return Container();
+    }
+
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15.0),
+        child: Container(
+          color: Colors.white,
+          child: Image.network(
+            restaurant.data['image_url'],
+            width: 175,
+            height: 175,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.all(10),
       child: StreamBuilder<QuerySnapshot>(
         stream: Firestore.instance.collection('food').snapshots(),
         builder: (context, snapshot) {
@@ -27,28 +59,36 @@ class _FoodBodyState extends State<FoodBody> {
             return LinearProgressIndicator();
             // return Text('Loading...');
           }
+          // print(snapshot.data.documents.length);
 
-          print(snapshot.data.documents.length);
+          // snapshot.data.documents.forEach((place) {
+          //   print(place.documentID);
+          //   print(place.data);
+          // });
 
-          snapshot.data.documents.forEach((place) {
-            print(place.documentID);
-            print(place.data);
-          });
-          
           return ListView.builder(
             itemCount: snapshot.data.documents.length,
             itemBuilder: (BuildContext context, int index) {
-              DocumentSnapshot resturant = snapshot.data.documents[index];
-              String resturantName = resturant.documentID.toString();
+              DocumentSnapshot restaurant = snapshot.data.documents[index];
+              DocumentSnapshot nextRestaurant;
+              // String restaurantName = restaurant.documentID.toString();
+              // print(index);
+              // print(snapshot.data.documents.length);
+              if (index + 1 < snapshot.data.documents.length) {
+                nextRestaurant = snapshot.data.documents[index + 1];
+              }
 
-              return ListTile(
-                title: Row(
+              if (index.isEven || index == snapshot.data.documents.length) {
+                return Row(
+                  mainAxisAlignment: nextRestaurant != null ? MainAxisAlignment.center : MainAxisAlignment.start,
                   children: <Widget>[
-                    Text(resturantName),
-                    Image.network(resturant.data['image_url'], width: 200.0,)
+                    _makeFoodCard(restaurant),
+                    _makeFoodCard(nextRestaurant),
                   ],
-                ),
-              );
+                );
+              }
+
+              return Container();
             },
           );
         },
