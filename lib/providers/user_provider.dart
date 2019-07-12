fec0a8b9435d1258b2f009b3a2ed2e65f8d8b30d
JaiMistry <jsmistry@asu.edu>
2019-07-12 15:02:05 -0700

    Added some settings and update function

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -6,6 +6,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 // Create a user object that is easier to work with
 class WorkItOffUser {
+  final Firestore _firestore = Firestore.instance; // Create firestore instance
   final String id;
   final String gender;
   final String age;
@@ -25,7 +26,24 @@ class WorkItOffUser {
   }
 
   set gender(String newGender) {
-    Firestore.instance.collection('users').document(id).updateData({'gender': newGender});
+    _firestore.collection('users').document(id).updateData({'gender': newGender});
+  }
+
+  set age(String newAge) {
+    _firestore.collection('users').document(id).updateData({'age': newAge});
+  }
+
+  set weight(String newWeight) {
+    _firestore.collection('users').document(id).updateData({'age': newWeight});
+  }
+
+  Future<void> updateProfile(String userID, String gender, int age, int weight) async {
+    // SharedPreferences prefs = await SharedPreferences.getInstance();
+    // prefs.setInt('age', age);
+    // prefs.setInt('weight', weight);
+    // prefs.setString('gender', gender);
+
+    return _firestore.collection('users').document(userID).updateData({'age': age, 'weight': weight, 'gender': gender});
   }
 }
 
