33c591f3338146847eceed88ce29943f60663a71
JaiMistry <jsmistry@asu.edu>
2019-07-23 14:40:29 -0700

    Data is now pulled from firebase

--- lib/pages/progress.dart
+++ lib/pages/progress.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:provider/provider.dart';
+
+import 'package:workitoff/providers/user_provider.dart';
 
 class ProgressPage extends StatefulWidget {
   @override
@@ -9,11 +13,11 @@ class ProgressPage extends StatefulWidget {
 }
 
 class _ProgressPageState extends State<ProgressPage> {
-  int _currentPercentage = 88;
-  int _calsRemaining = 65;
+  // int _currentPercentage = 88;
+  // double _calsRemaining = 0.65;
   bool _progressMode = true;
 
-  Widget _renderText() {
+  Widget _renderText(_currentPercentage, _calsRemaining) {
     if (_progressMode) {
       return RichText(
         textAlign: TextAlign.center,
@@ -51,6 +55,17 @@ class _ProgressPageState extends State<ProgressPage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    WorkItOffUser user = Provider.of<WorkItOffUser>(context);
+    int _calsRemaining = 0;
+    int _currentPercentage = 0;
+    if (user != null) {
+      _calsRemaining = (user.calsAdded - user.calsBurned);
+      _currentPercentage = (user.calsBurned / user.calsAdded * 100).round();
+    }
+
+    // print(user.calsAdded);
+    // print(user.calsBurned);
+    // print(_currentPercentage);
 
     return Container(
       height: double.infinity,
@@ -63,54 +78,56 @@ class _ProgressPageState extends State<ProgressPage> {
           stops: const [0.75, 1],
         ),
       ),
-      child: SafeArea(
-        child: Container(
-          padding: EdgeInsets.only(top: 30, bottom: 30, left: 10, right: 10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset('assets/text_logo.png', width: 80),
-              const SizedBox(height: 30),
-              CircularPercentIndicator(
-                radius: size.width * 0.75,
-                lineWidth: 21.0,
-                percent: _currentPercentage * .01,
-                center: Text(
-                  '$_currentPercentage%',
-                  style: TextStyle(fontSize: 45),
-                ),
-                progressColor: const Color(0xff3ADEA7),
-                backgroundColor: Colors.purple.withOpacity(0.1),
-                reverse: true,
-              ),
-              const SizedBox(height: 15),
-              _renderText(),
-              const SizedBox(height: 20),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: OutlineButton(
-                    child: Text(
-                      _progressMode ? 'Show Calories' : 'Show Progress',
-                      style: TextStyle(color: Colors.white),
+      child: (user == null)
+          ? Container()
+          : SafeArea(
+              child: Container(
+                padding: EdgeInsets.only(top: 30, bottom: 30, left: 10, right: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image.asset('assets/text_logo.png', width: 80),
+                    const SizedBox(height: 30),
+                    CircularPercentIndicator(
+                      animateFromLastPercent: true,
+                      animation: true,
+                      animationDuration: 700,
+                      radius: size.width * 0.75,
+                      lineWidth: 21.0,
+                      percent: _currentPercentage * .01,
+                      center: Text('$_currentPercentage%', style: TextStyle(fontSize: 45)),
+                      progressColor: const Color(0xff3ADEA7),
+                      backgroundColor: Colors.purple.withOpacity(0.1),
+                      reverse: true,
                     ),
-                    onPressed: () {
-                      setState(() {
-                        _progressMode = !_progressMode;
-                      });
-                    },
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
-                    borderSide: const BorderSide(color: Color(0xff17e3f1)),
-                    highlightColor: const Color(0xff17e3f1),
-                    highlightedBorderColor: const Color(0xff17e3f1),
-                    splashColor: Colors.transparent,
-                  ),
+                    const SizedBox(height: 15),
+                    _renderText(_currentPercentage, _calsRemaining),
+                    const SizedBox(height: 20),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: OutlineButton(
+                          child: Text(
+                            _progressMode ? 'Show Calories' : 'Show Progress',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _progressMode = !_progressMode;
+                            });
+                          },
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                          borderSide: const BorderSide(color: Color(0xff17e3f1)),
+                          highlightColor: const Color(0xff17e3f1),
+                          highlightedBorderColor: const Color(0xff17e3f1),
+                          splashColor: Colors.transparent,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
